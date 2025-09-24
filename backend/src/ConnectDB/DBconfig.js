ac841205e28afc1ad1a48abd7ba78c7ead9f3816
vicious-franco <irakaramale@gmail.com>
2025-09-24 10:51:38 +0200

    change database .env connection string

--- backend/src/ConnectDB/DBconfig.js
+++ backend/src/ConnectDB/DBconfig.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 export const connnectDB = async () => {
   try {
-    await mongoose.connect(process.env.LOCAL_DB_CONNECTION_STR);
+    await mongoose.connect(process.env.CLOUD_DB_CONNECTION_STR);
     console.log(mongoose.connection.host);
   } catch (err) {
     console.log("Internal error: ", err);
